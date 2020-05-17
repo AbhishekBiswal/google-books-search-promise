873e6e187f04d57e9142bfa989c96da1529e16c2
Abhishek Biswal <wirddin@gmail.com>
2020-05-18 03:09:27 +0530

    change test to use then/catch

--- test/search-test.js
+++ test/search-test.js
@@ -5,8 +5,7 @@ var books = require('../lib/google-books-search.js');
 describe('Search', function() {
 
     it('should return a JSON object of books', function(done) {
-        books.search('Guinness World Records', {}, function(error, results) {
-            should.not.exist(error);
+        books.search('Guinness World Records', {}).then(function({results}) {
             results.should.be.an.instanceof(Array)
             results[0].should.have.property('title');
             done();
@@ -14,8 +13,7 @@ describe('Search', function() {
     });
 
     it('the options argument should be optional', function(done) {
-        books.search('Guinness World Records', function(error, results) {
-            should.not.exist(error);
+        books.search('Guinness World Records').then(function({results}) {
             results.should.be.an.instanceof(Array)
             results[0].should.have.property('title');
             done();
@@ -23,8 +21,7 @@ describe('Search', function() {
     });
 
     it('should return an empty array if there are no results', function(done) {
-        books.search('JCEhrrpxF2E1s7aPW8zd2903tQ4AlCB9', {}, function(error, results) {
-            should.not.exist(error);
+        books.search('JCEhrrpxF2E1s7aPW8zd2903tQ4AlCB9', {}).then(function({results}) {
             results.should.be.an.instanceof(Array)
             results.length.should.equal(0);
             done();
@@ -34,8 +31,7 @@ describe('Search', function() {
     it('should return a specified number of results', function(done) {
         books.search('Guinness World Records', {
             limit: 15
-        }, function(error, results) {
-            should.not.exist(error);
+        }).then(function({results}) {
             results.length.should.equal(15);
             done();
         });
@@ -44,23 +40,27 @@ describe('Search', function() {
     it('should only accept an limit below 40', function(done) {
         books.search('Guinness World Records', {
             limit: 50
-        }, function(error, results) {
+        }).then(function({results}) {
+            throw new Error("Limit > 40")
+            done();
+        }).catch(function(error) {
             should.exist(error);
             done();
         });
     });
 
     it('should return an error if no query is specified', function(done) {
-        books.search(null, {}, function(error, results) {
-            should.exist(error);
+        books.search(null, {}).then(function({results}) {
             should.not.exist(results);
+            done();
+        }).catch(function(error) {
+            should.exist(error);
             done();
         });
     });
 
     it('should return the full API response body', function(done) {
-        books.search('Javascript', {}, function(error, results, response) {
-            should.not.exist(error);
+        books.search('Javascript', {}).then(function({results, response}) {
             should.exist(response);
             should.equal(response.kind, 'books#volumes');
             should.exist(response.items);
@@ -74,8 +74,7 @@ describe('Search', function() {
 describe('Lookup', function() {
 
     it('should return a JSON object of a volume', function(done) {
-        books.lookup('9KJJYFIss_wC', function(error, result) {
-            should.not.exist(error);
+        books.lookup('9KJJYFIss_wC').then(function({result}) {
             result.should.be.an.instanceof(Object);
             result.id.should.equal('9KJJYFIss_wC');
             result.should.have.property('title');
@@ -84,9 +83,11 @@ describe('Lookup', function() {
     });
 
     it('should return an error if an invalid volume id is provided', function(done) {
-        books.lookup('this is not a real volume id', function(error, result) {
-            should.exist(error);
+        books.lookup('this is not a real volume id').then(function({result}) {
             should.not.exist(result);
+            done();
+        }).catch(function(error) {
+            should.exist(error);
             done();
         });
     });
